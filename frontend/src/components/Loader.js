ba0437d5e37e27b01d4f0998b01e5e0931f1c6f4
mustakindev <devmustakin@gmail.com>
2025-09-18 16:20:35 +0600

    Create Loader.js

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="flex space-x-2 justify-center items-center my-10">
+      <span className="w-4 h-4 bg-pink-400 rounded-full animate-bounce"></span>
+      <span className="w-4 h-4 bg-purple-400 rounded-full animate-bounce delay-150"></span>
+      <span className="w-4 h-4 bg-blue-400 rounded-full animate-bounce delay-300"></span>
+    </div>
+  );
+}
